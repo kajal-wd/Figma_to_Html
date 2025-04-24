7551daee0f00b802c473feb2515ae8c131fa78dd
kajal <ks7044566@gmail.com>
2025-04-24 22:21:30 +0530

    updated

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const navBtn = document.getElementById("nav-btn-toggle");
+const navBar = document.getElementById("navbar");
+const closeBtn = document.getElementById("close-navbar");
+console.log(navBtn); 
+navBtn.addEventListener("click",()=>{
+    navBar.classList.add("active-navbar");
+    
+})
 
-  
+closeBtn.addEventListener("click",()=>{
+    navBar.classList.remove("active-navbar");
+})
